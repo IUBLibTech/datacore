d0be89566f1b0b33e51a104f8ebd54afce6c95ce
Adam Ploshay <aploshay@iu.edu>
2024-12-03 09:39:10 -0500

    [IUS-2537] update 404 action and html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -105,4 +105,9 @@ class ApplicationController < ActionController::Base
     auth.request.flash.clear
   end
 
+  rescue_from ActionController::UnknownFormat, with: :rescue_404
+
+  def rescue_404
+    render file: Rails.public_path.join('404.html'), status: :not_found, layout: false
+  end
 end
