773b851b998e2d68a1d178b569deac37c94b420d
Adam Ploshay <aploshay@iu.edu>
2025-02-11 15:59:10 -0500

    [IUS-2584] monkeypatch Collections authority for searching partial matches

--- lib/extensions/qa/authorities/collections/collections_search.rb
+++ lib/extensions/qa/authorities/collections/collections_search.rb
@@ -1,4 +1,4 @@
-# unmodified from hyrax 2.9.6
+# modified from hyrax 2.9.6
 module Extensions
   module Qa
     module Authorities
@@ -8,6 +8,8 @@ module Extensions
             # The Hyrax::CollectionSearchBuilder expects a current_user
             return [] unless controller.current_user
             repo = ::CatalogController.new.repository
+            # line below facilitates searching on partial word match
+            controller.params[:q] << '*' if controller.params[:q].to_s.size >= 2
             builder = search_builder(controller)
             response = repo.search(builder)
             docs = response.documents
