d08b38862e5387226fa1cd4354e025b6b2024c51
Adam Ploshay <aploshay@iu.edu>
2025-02-13 15:59:28 -0500

    [IUS-2586] prep controller specs for redirect changes

--- spec/controllers/hyrax/collections_controller_spec.rb
+++ spec/controllers/hyrax/collections_controller_spec.rb
@@ -2,10 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Hyrax::CollectionsController do
   let(:subject) { described_class.new }
+  let(:collection) { FactoryBot.create(:collection_lw) }
 
   describe "#render_bookmarks_control?" do
     it "returns false" do
       expect(subject.send(:render_bookmarks_control?)).to eq false
+    end
+  end
+
+  describe "#display_provenance_log" do
+    it "redirects" do
+      get :display_provenance_log, params: { id: collection.id }
+      expect(response).to be_redirect
     end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,6 +55,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include Devise::Test::ControllerHelpers, type: :controller
 
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
