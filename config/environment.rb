99c4c51e41492ef41acd84c1536d1feaed98084f
Adam Ploshay <aploshay@iu.edu>
2025-03-27 14:21:53 -0400

    [IUS-2580] display release in github footer, move commit to comment

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,8 @@
 require_relative 'application'
 
 # load commit version for footer
-ENV['SOURCE_COMMIT'] = `git rev-parse --short HEAD` unless ENV['SOURCE_COMMIT'].present?
+ENV['SOURCE_COMMIT'] = `git rev-parse --short HEAD`.squish unless ENV['SOURCE_COMMIT'].present?
+ENV['SOURCE_VERSION'] = `git describe --tags --abbrev=0`.squish unless ENV['SOURCE_VERSION'].present?
 
 # Initialize the Rails application.
 Rails.application.initialize!
