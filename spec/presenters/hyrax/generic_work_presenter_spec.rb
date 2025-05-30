12da388089467cb72e096ce3cb04313954398ee0
Renee LeBeau <rdlebeau@iu.edu>
2025-05-30 18:35:33 -0400

    [IUS-2613] DataCORE presenter unit tests

--- spec/presenters/hyrax/generic_work_presenter_spec.rb
+++ spec/presenters/hyrax/generic_work_presenter_spec.rb
@@ -1,9 +1,22 @@
-# Generated via
-#  `rails generate hyrax:work GenericWork`
 require 'rails_helper'
 
 RSpec.describe Hyrax::GenericWorkPresenter do
-  it "has tests" do
-    skip "Add your tests here"
+  let(:user) { FactoryBot.create :user }
+  let(:attributes) do {} end
+  let(:solr_document) { SolrDocument.new(attributes) }
+  let(:current_ability) { instance_double(Ability, current_user: user ) }
+
+  subject { described_class.new(solr_document, current_ability) }
+
+  describe "delegates methods to solr_document:" do
+    [:identifier_orcid, :academic_affiliation, :other_affiliation, :contributor_affiliationumcampus, :alt_title, :date_issued,
+     :identifier_source, :peerreviewed, :bibliographic_citation, :relation_ispartofseries, :rights_statement, :type_none,
+     :language_none, :description_mapping, :description_abstract, :description_sponsorship, :description].each do
+    |method|
+      it "#{method}" do
+        expect(subject).to delegate_method(method).to(:solr_document)
+      end
+    end
   end
+
 end
