8979e2c2545ea2b5aaa38598c05c4ee19f5e1cdb
Adam Ploshay <aploshay@iu.edu>
2025-06-05 13:00:09 -0400

    [IUS-2626] prepare monkeypatch for CollapsableSectionPresenter

---
+++ lib/extensions/hyrax/collapsable_section_presenter/collapsable_section_presenter_behavior.rb
@@ -0,0 +1,22 @@
+# unmodified from hyrax 2.9.6
+module Extensions
+  module Hyrax
+    module CollapsableSectionPresenter
+      module CollapsableSectionPresenterBehavior
+        private
+        def button_tag
+          content_tag(:a,
+                      role: 'button',
+                      class: "#{button_class}collapse-toggle",
+                      data: { toggle: 'collapse' },
+                      href: "##{id}",
+                      'aria-expanded' => open,
+                      'aria-controls' => id) do
+                        safe_join([content_tag(:span, '', class: icon_class, 'aria-hidden' => true),
+                                   content_tag(:span, text)], ' ')
+                      end
+        end
+      end
+    end
+  end
+end
