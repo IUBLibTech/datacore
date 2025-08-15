e884afa65e5f568f50764f8edb728070b9414885
Adam Ploshay <aploshay@iu.edu>
2025-08-15 11:01:45 -0400

    [IUS-2749] revise rspec example, suite record cleanup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -26,16 +26,17 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f } # rubocop:di
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
-
+ActiveJob::Base.queue_adapter = :test
 
 RSpec.configure do |config|
+  config.include ActiveJob::TestHelper
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = false # true
+  config.use_transactional_fixtures = true
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -58,29 +59,72 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   config.include Devise::Test::ControllerHelpers, type: :controller
 
+# Databse, Fedora cleaning
+#
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
   end
 
-  config.before do
-    DatabaseCleaner.strategy = :transaction
+ config.around do |example|
+    #  Sometimes tests need a clean Fedora and Solr environment to work properly. To invoke the ActiveFedora
+    #  cleaner use the :clean metadata directive like:
+    #   describe "#structure", :clean do
+    #      some example
+    #    end
+    if example.metadata[:clean]
+      ActiveFedora::Cleaner.clean!
+      ActiveFedora.fedora.connection.send(:init_base_path) if example.metadata[:js]
+
+      # Recreate only the AdminSet, not the associated permission template that is still in the database.
+      # (Instead of AdminSet.find_or_create_default_admin_set_id)
+      if AdminSet.count.zero?
+        AdminSet.create id: AdminSet::DEFAULT_ID, title: Array.wrap(AdminSet::DEFAULT_TITLE)
+      end
+      # recreate permission template if it's not in the database, after all
+      if ActiveRecord::Base.connection.table_exists?('permission_templates') && Hyrax::PermissionTemplate.count.zero?
+        hascs = Hyrax::AdminSetCreateService.new(admin_set: AdminSet.first, creating_user: nil)
+        pt = hascs.send :create_permission_template
+        workflow = hascs.send :create_workflows_for, permission_template: pt
+        access = hascs.send :create_default_access_for, permission_template: pt, workflow: workflow
+      end
+
+      # Let the DatabaseCleaner take care of database rows written in an example
+      if example.metadata[:type] == :feature && Capybara.current_driver != :rack_test
+        DatabaseCleaner.strategy = :truncation
+      else
+        DatabaseCleaner.strategy = :transaction
+        DatabaseCleaner.start
+      end
+
+      DatabaseCleaner.cleaning do
+        example.run
+      end
+    else
+      example.run
+    end
   end
 
-  config.before(:each, js: true) do
-    DatabaseCleaner.strategy = :truncation
+  config.after(:each) do |example|
+    if example.metadata[:js]
+      save_timestamped_page_and_screenshot(Capybara.page, example.metadata) if example.exception
+    end
   end
 
-  # This block must be here, do not combine with the other `before(:each)` block.
-  # This makes it so Capybara can see the database.
-  config.before do
-    DatabaseCleaner.start
+  config.after(:each, type: :feature) do
+    Warden.test_reset!
+    Capybara.reset_sessions!
+    page.driver.reset!
+    # Keep up to the number of screenshots specified in the hash
+    Capybara::Screenshot.prune_strategy = { keep: 10 }
   end
 
   config.after do
     DatabaseCleaner.clean
   end
 
-  config.after(:each) do
-    ActiveFedora::Cleaner.clean! if Settings.rspec.aggressive_cleaning
+  config.after(:suite) do
+    # Clean everything
+    DatabaseCleaner.clean
+    ActiveFedora::Cleaner.clean!
   end
 end
