fbfa95001ca47d864e9274934a8e5f4828197e36
Renee LeBeau <rdlebeau@iu.edu>
2025-08-21 18:04:13 -0400

    IUS-2613 method name spelling correction

--- app/controllers/hyrax/data_sets_controller.rb
+++ app/controllers/hyrax/data_sets_controller.rb
@@ -17,7 +17,7 @@ module Hyrax
     before_action :assign_admin_set,             only: %i[create update]
     before_action :workflow_destroy,             only: [:destroy]
     before_action :provenance_log_update_before, only: [:update]
-    before_action :visiblity_changed,            only: [:update]
+    before_action :visibility_changed, only: [:update]
     before_action :prepare_permissions,          only: [:show]
 
     after_action :workflow_create,               only: [:create]
