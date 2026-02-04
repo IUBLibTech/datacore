c5204381898ff16220b87b0f289a6f7ace5d24ee
Adam Ploshay <aploshay@iu.edu>
2026-02-04 08:10:34 -0500

    [IULRDC-87][IUS-2747] require recaptcha in contact form

--- lib/extensions/hyrax/contact_form_controller/contact_form_recaptcha.rb
+++ lib/extensions/hyrax/contact_form_controller/contact_form_recaptcha.rb
@@ -3,10 +3,12 @@ module Extensions
     module ContactFormController
       module ContactFormRecaptcha
 
-        # unmodified from hyrax 2.9.6
+        # modified from hyrax 2.9.6 with recaptcha check
         def create
+          if !recaptcha_success?
+            flash.now[:alert] = 'Please verify that you are not a robot.'
           # not spam and a valid form
-          if @contact_form.valid?
+          elsif @contact_form.valid?
             ::Hyrax::ContactMailer.contact(@contact_form).deliver_now
             flash.now[:notice] = 'Thank you for your message!'
             after_deliver
@@ -21,10 +23,17 @@ module Extensions
         end
 
         private
-          # unmodified from hyrax 2.9.6
+          # modified from hyrax 2.9.6 with recaptcha params
           def contact_form_params
             return {} unless params.key?(:contact_form)
-            params.require(:contact_form).permit(:contact_method, :category, :name, :email, :subject, :message)
+            params.require(:contact_form).permit(:contact_method, :category, :name, :email, :subject, :message, 'g-recaptcha-response'.to_sym, 'g-recaptcha-response-data'.to_sym => [:contact_form])
+          end
+
+          def recaptcha_success?
+            return true unless Settings.archive_api.use_recaptcha
+            v3_success = verify_recaptcha(action: 'contact_form', minimum_score: Settings.recaptcha.minimum_score.to_f, secret_key: Settings.recaptcha.v3.secret_key)
+            v2_success = verify_recaptcha unless v3_success
+            v3_success || v2_success
           end
       end
     end
