99352d9137acbd1408d6869f84a21c625add2e6e
Adam Ploshay <aploshay@iu.edu>
2026-08-12 15:31:40 -0400

    move search, bookmark controller patches into modules

--- lib/extensions/hyrax/collections_controller/render_bookmarks_control.rb
+++ lib/extensions/hyrax/collections_controller/render_bookmarks_control.rb
@@ -3,8 +3,9 @@ module Extensions
     module CollectionsController
       module RenderBookmarksControl
         protected
-
         # disable the bookmark control from displaying in gallery view
+        # Hyrax doesn't show any of the default controls on the list view, so
+        # this method is not called in that context.
         def render_bookmarks_control?
           false
         end
